1bb02149e3cc6219d857c69ed3e2ad40c4bb2edb
Zachary Jones <zacharyj@jonesserver.online>
2024-03-09 11:18:59 -0600

    added get command and other things

---
+++ Fusion Games X CLI/main.py
@@ -0,0 +1,17 @@
+#start CLI backend
+
+import os #command runner
+
+#run command: os.system("ls -l")
+
+
+def get():
+    q1 = str(input("Branch? (case sensitive)"))
+    Main = "main", "Main"
+    Hosting = "Hosting", "Host", "hostingproviders"
+    if q1 == Main:
+        print("cloning from main branch.")
+        os.system("git clone -b main https://gitlab.com/fusiongames1/fusio-games-x.git")
+    elif q1 == Hosting:
+        print("cloning hosting branch (for hosting locally)")
+        os.system("git clone -b hostingproviders https://gitlab.com/fusiongames1/fusion-games-x.git")
